404e562b2989fcfda588595411f7844faeb69db4
Weiqifan1 <cmlykke@hotmail.com>
2023-06-16 21:48:58 +0200

    2023-06-16_customDataGenerator

---
+++ README.md
@@ -0,0 +1,8 @@
+In order to use the program, a few extra files are needed.
+in the folder
+it is a file named ids.txt
+you can get it here: https://github.com/cjkvi/cjkvi-ids
+
+and these two files:
+Junda2005.txt and 
+Tzai2006.txt 
---
+++ src/main/java/org/example/CustomDataGenerator/CustomDataGeneratorMain.java
@@ -0,0 +1,9 @@
+package org.example.CustomDataGenerator;
+
+public class CustomDataGeneratorMain {
+    public static void main(String[] args) {
+        System.out.println("Custom data generator started!");
+
+        System.out.println("Custom data generator ended!");
+    }
+}
